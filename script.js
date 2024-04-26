abf26eebf2af747500793680c1a696764b2a05f1
Khab <unitetempete@gmail.com>
2024-04-26 16:46:38 +0200

    Adding one function to randomize a choice for the computer and another one for inputting the human choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Test")
+let choices = ["rock", "paper", "scissors"];
+let humanScore, computerScore = 0;
+
+
+function getComputerChoice(){
+    return choices[Math.floor(Math.random() * 2)]
+}
+
+function getHumanChoice(){
+    userChoice = prompt("Choose between paper, rock or scissors:")
+    return userChoice
+}
+
+console.log(getComputerChoice())
+console.log(getHumanChoice())
