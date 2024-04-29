f5b43e64e6cac08d1232b6955a024a5218d7c295
Khab <unitetempete@gmail.com>
2024-04-29 23:13:05 +0200

    Refactoring, deleting all unused functions, deleting useless references, currently broken

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
 const CHOICES = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+let choices = document.querySelectorAll(".button-container input");
+let result = document.querySelector(".result-container");
 
+choices.forEach(function (item) {
+    item.addEventListener("click", (event) => {
+        humanChoice = event.target.id;
+    });
+});
 
-function capitalize(str){
-    return str[0].toUpperCase() + str.slice(1)
-}
 
 function getComputerChoice(){
     return CHOICES[Math.floor(Math.random() * 2)]
 }
 
-function getHumanChoice(){
-    userChoice = prompt("Choose between paper, rock or scissors:");
-    return userChoice
-}
-
 
 function playRound(humanChoice, computerChoice){
 
-    if (capitalize(humanChoice) == "Rock" && computerChoice == "scissors"){
-        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+    if (humanChoice == "rock" && computerChoice == "scissors"){
+        result.textContent = `You win, ${humanChoice} beats ${computerChoice}!`;
         humanScore++;
 
-    } else if (capitalize(humanChoice) == "Rock" && computerChoice == "paper") {
-        console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+    } else if (humanChoice == "rock" && computerChoice == "paper") {
+        result.textContent = `You lose, ${computerChoice} beats ${humanChoice}!`;
         computerScore++;
 
-    } else if (capitalize(humanChoice) == "Paper" && computerChoice == "rock") {
-        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        result.textContent = `You win, ${humanChoice} beats ${computerChoice}!`;
         humanScore++;
 
-    } else if (capitalize(humanChoice) == "Paper" && computerChoice == "scissors") {
-        console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        result.textContent = `You lose, ${computerChoice} beats ${humanChoice}!`;
         computerScore++;
 
-    } else if (capitalize(humanChoice) == "Scissors" && computerChoice == "paper") {
-        console.log(`You win, ${humanChoice} beats ${computerChoice}!`);
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        result.textContent = `You win, ${humanChoice} beats ${computerChoice}!`;
         humanScore++;
 
-    } else if (capitalize(humanChoice) == "Scissors" && computerChoice == "rock") {
-        console.log(`You lose, ${computerChoice} beats ${humanChoice}!`);
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        result.textContent = `You lose, ${computerChoice} beats ${humanChoice}!`;
         computerScore++;
 
     } else {
-        console.log(`It's a draw, you choosed ${humanChoice} as the computer!`);
+        result.textContent = `It's a draw, you choosed ${humanChoice} as the computer!`;
     }
 }
 
 
-if (humanScore > computerScore) {
-    console.log("Victory!")
-} else {
-    console.log("Defeat, too bad...")
-}
-
+playRound(humanChoice, getComputerChoice());
